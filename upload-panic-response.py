8cfd22d62ebe7ae295a0ec10e2fde37e875856f9
Ritikashar <ritika.s@hotstar.com>
2022-11-17 09:45:17 +0530

    changes for file path, request path

--- upload-panic-response.py
+++ upload-panic-response.py
@@ -5,75 +5,55 @@ import sys
 import requests
 
 
-ftp_username = os.environ.get('FTP_USERNAME','')
-ftp_password = os.environ.get('FTP_PASSWORD','')
-ftp_hostname = os.environ.get('FTP_HOSTNAME','')
-netstorage_path = os.environ.get('NETSTORAGE_PATH','')
+akamai_netstorage_ftp_username = os.environ.get('AKAMAI_NETSTORAGE_FTP_USERNAME','') 
+akamai_netstorage_ftp_password = os.environ.get('AKAMAI_NETSTORAGE_FTP_PASSWORD','')
+akamai_netstorage_ftp_hostname = os.environ.get('AKAMAI_NETSTORAGE_FTP_HOSTNAME','') 
+akamai_netstorage_ftp_path = os.environ.get('AKAMAI_NETSTORAGE_PATH','') 
 
-api_name = None
-host = None
-country_code = None
 
-headers = {
-    'authority':'webx.hotstar.com',
-    'accept':'application/json, text/plain, */*',
-    'accept-language':'eng',
-    'cache-control':'no-cache',
-    'hotstarauth':'st=1668236089~exp=1668242089~acl=/*~hmac=ff38e75337d7970078b99ac1a1b4d60562ed5b1265cc77b880b2b9e27450da0f',
-    'pragma':'no-cache',
-    'referer':'https://webx.hotstar.com/ph/home',
-    'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-    'x-country-code':'ph',
-    'x-hs-accept-language':'eng',
-    'x-hs-platform':'web'
-}
 
+request_path = None #rp 
+cluster = None # c 
+request_type = None # t
+file_storage_path = None #fp
+data = None
+headers = None 
+query_params = None
 
 
 argumentList = sys.argv[1:]
  
-# Options
-#TODO add optional parmaters for query params
-options = "a:h:c:q"
-
-
- 
-# Long options
-long_options = ["API", "Host", "Country_code"]
+options = "r:c:t:f:"
+long_options = ["RequestPath", "Cluster", "RequestType", "FileStoragePath"]
  
 try:
-    # Parsing argument
     arguments, values = getopt.getopt(argumentList, options, long_options)
      
-    # checking each argument
     for currentArgument, currentValue in arguments:
  
-        if currentArgument in ("-a", "--API"):
-            api_name = currentValue
-            print ("API " , currentValue)
+        if currentArgument in ("-r", "--RequestPath"):
+            request_path = currentValue
+            print ("RequestPath " , currentValue)
              
-        elif currentArgument in ("-h", "--Host"):
-            host = currentValue
+        elif currentArgument in ("-c", "--Cluster"):
+            cluster = currentValue
             print ("Host = ", currentValue)
         
-        elif currentArgument in ("-c", "--Country_code"):
-            country_code = currentValue
-            print("Country code = ", country_code)
-
+        elif currentArgument in ("-t", "--RequestType"):
+            request_type = currentValue
+            print("Request Type = ", request_type)
         
-             
-             
+        elif currentArgument in ("-f", "--FileStoragePath"):
+            file_storage_path = currentValue
+            print("FileStoragePath = ", file_storage_path)
+
 except getopt.error as err:
-    # output error, and return with an error code
     print (str(err))
 
-request_url = host+api_name
-response = requests.get(request_url, headers=headers)
+
+response = requests.get(request_path, headers=headers)
 print(response)
 
-
-#connect to NS to store the file
-#Net storage is from env variable + folder name from api name & country code
         
 
       
@@ -84,4 +64,4 @@ print(response)
 
 
 
-
+#curl -v 'https://webx.hotstar.com/api/internal/bff/v2/slug/ph/paywall'   -H 'authority: webx.hotstar.com'   -H 'accept: application/json, text/plain, */*'   -H 'accept-language: eng'   -H 'cache-control: no-cache'   -H 'hotstarauth: st=1668236089~exp=1668242089~acl=/*~hmac=ff38e75337d7970078b99ac1a1b4d60562ed5b1265cc77b880b2b9e27450da0f'   -H 'pragma: no-cache'   -H 'referer: https://webx.hotstar.com/ph/home'   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'   -H 'x-country-code: ph'   -H 'x-hs-accept-language: eng'   -H 'x-hs-platform: web'
