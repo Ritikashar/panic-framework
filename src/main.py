0ea2e4179a6185fbd96085a1c8e6cedfa7216dc1
Ritikashar <ritika.s@hotstar.com>
2022-11-17 17:31:32 +0530

    code cleanup

--- src/main.py
+++ src/main.py
@@ -8,34 +8,14 @@ from dotenv import dotenv_values
 from ftplib import FTP, FTP_TLS
 import configparser
 
-def main():
-    environment_name = None 
-    argumentList = sys.argv[1:]
-    options = "e:"
-    long_options = ["EnviornmentName"]
-    try:
-        arguments, values = getopt.getopt(argumentList, options, long_options)
-        for currentArgument, currentValue in arguments:
-            if currentArgument in ("-e", "--EnviornmentName"): #continue from cmd line
-                environment_name = currentValue
-    except getopt.error as err:
-        print (str(err))
-
-    akamai_config = get_akamai_config() 
-    environment_config = get_environment_config(environment_name)
-    panic_response = create_panic_response_file(akamai_config,environment_config)
-    upload_panic_response(panic_response,akamai_config,environment_config)
-
-if __name__ == "__main__":
-    main()
 
 def get_akamai_config():
     akamai_config = dotenv_values(".env")
-    akamai_config = {
-        "akamai_netstorage_ftp_username" : config.get('AKAMAI_NETSTORAGE_FTP_USERNAME') ,
-        "akamai_netstorage_ftp_password" : config.get('AKAMAI_NETSTORAGE_FTP_PASSWORD'),
-        "akamai_netstorage_ftp_hostname" :  config.get('AKAMAI_NETSTORAGE_FTP_HOSTNAME') ,
-        "akamai_netstorage_folder_path" : config.get('AKAMAI_NETSTORAGE_FOLDER_PATH'),
+    return {
+        "akamai_netstorage_ftp_username" : akamai_config.get('AKAMAI_NETSTORAGE_FTP_USERNAME') ,
+        "akamai_netstorage_ftp_password" : akamai_config.get('AKAMAI_NETSTORAGE_FTP_PASSWORD'),
+        "akamai_netstorage_ftp_hostname" : akamai_config.get('AKAMAI_NETSTORAGE_FTP_HOSTNAME') ,
+        "akamai_netstorage_folder_path"  : akamai_config.get('AKAMAI_NETSTORAGE_FOLDER_PATH'),
     }
     
     return akamai_config
@@ -47,7 +27,8 @@ def get_environment_config(environment_name):
         "cluster":environment_config.get('CLUSTER'),
         "request_url":environment_config.get('REQUEST_URL'),
         "header_file_location":environment_config.get('HEADER_FILE_PATH'),
-        "request_type":environment_config.get('REQUEST_TYPE')
+        "request_type":environment_config.get('REQUEST_TYPE'),
+        "header_file_location":environment_config.get('HEADER_FILE_LOCATION')
     }
 
 def get_headers(header_file_location):
@@ -60,7 +41,9 @@ def create_panic_response_file(akamai_config, environment_config):
     response = None
     request_type = environment_config.get("REQUEST_TYPE", None)
     request_url = environment_config.get("REQUEST_URL", None)
-    headers = get_headers(environment_config.get("HEADER_FILE_PATH", None))
+    header_file = open(environment_config.get("HEADER_FILE_LOCATION", None))
+    headers = header_file.read()
+    header_file.close()   
     if request_type in ("get", "GET", "Get"):
         response = requests.get(request_url, headers=headers)
     elif request_type in ("post", "POST", "Post"):
@@ -86,9 +69,24 @@ def upload_panic_response(panic_response,akamai_config,environment_config):
       
     
 
+def main():
+    environment_name = None 
+    argumentList = sys.argv[1:]
+    options = "e:"
+    long_options = ["EnviornmentName"]
+    try:
+        arguments, values = getopt.getopt(argumentList, options, long_options)
+        for currentArgument, currentValue in arguments:
+            if currentArgument in ("-e", "--EnviornmentName"): #continue from cmd line
+                environment_name = currentValue
+    except getopt.error as err:
+        print (str(err))
 
+    akamai_config = get_akamai_config() 
+    environment_config = get_environment_config(environment_name)
+    panic_response = create_panic_response_file(akamai_config,environment_config)
+    upload_panic_response(panic_response,akamai_config,environment_config)
 
-
-
-
+if __name__ == "__main__":
+    main()
 
